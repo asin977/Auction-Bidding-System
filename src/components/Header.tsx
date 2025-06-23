2450b379f096def6609264e9281241d566abf8a1
Assin Anna <assinannameckal@gmail.com>
2025-06-23 11:26:29 +0530

    Feat:Implemented the Header.

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/Header.css';
+import MyImage from '../images/Logo.png'
+import SignIcon from '../images/avatar.png'
+
+export const Header=() => {
+    return (
+        <div className='header-main-container'>
+            <div className='container'>
+                <div className='img-container'>
+                  <span className='site-name'>GravelGood</span>
+                  
+                    <img src={MyImage} alt="logo" />
+                </div>
+                <div className='sign-icon'>
+                      <img src={SignIcon} alt='menu'className='sign-img'/>
+                </div>
+            </div>
+        </div>
+       
+    )
+}
+
+export default Header
