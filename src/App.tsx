f5b9ea94896aaa3bb0ca892d06976cede985246b
Assin Anna <assinannameckal@gmail.com>
2025-06-24 20:18:58 +0530

    refractor:updated the code on review based.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Content} from './components/Content';
+import { SearchIcon } from './components/search';
 
 function App() {
   return (
     <div className="App">
-      <Content />
+      <SearchIcon />
     </div>
   );
 }
