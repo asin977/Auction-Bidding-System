2e9cc728a87123634610583f8aca95521ae4f666
asin977 <assinannameckal@gmail.com>
2025-06-24 17:51:09 +0530

    Feat: Feature/Sign-In (#1)
    * feat: completed sign-In page
    
    * update:file formats
    
    * updated the styles
    
    * upadate:comments resolved based on review.
    
    * Add product details dummy data. (#2)
    
    * Feat: Implemented data structure in Home page
    
    * update:Changed the data of products to json file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import './App.css';
+import { SignInPage } from './pages/SignInPage';
 
 function App() {
   return (
     <div className="App">
-     
+       <SignInPage />
     </div>
   );
 }
---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,12 @@
+import {SignIn} from '../components/signin';
+import {SiteDetails} from '../components/sitedetails';
+import {Footer} from '../components/footer';
+
+export const SignInPage =()=> (
+  <div>
+      <SignIn />
+      <SiteDetails />
+      <Footer />
+  </div>
+)
+
