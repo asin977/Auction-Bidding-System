a9a93f540067d5f2c562513f2ea89aa7280201e9
Assin Anna <assinannameckal@gmail.com>
2025-06-24 09:46:34 +0530

    feat:Implemented collectionDisplay component and its functionIity on the home page.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import CollectionDisplay from "../components/collectionDisplay"
+
+export const HomePage = () => {
+  return (
+    <div className="one">
+      <CollectionDisplay />
+    </div>
+  )
+}
+
+export default HomePage
