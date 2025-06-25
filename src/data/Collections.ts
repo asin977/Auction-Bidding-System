22432dfdebcbacc523ba10db1a0bedc7b2134ea3
asin977 <assinannameckal@gmail.com>
2025-06-25 09:24:11 +0530

    feat:Implemented collectionDisplay component and its functionIity on the Home page. (#6)
    * feat:Implemented collectionDisplay component and its functionIity on the home page.
    
    * upadate:change in folder format

---
+++ src/data/Collections.ts
@@ -0,0 +1,38 @@
+export type Collection = {
+  id: number;
+  imageUrl: string;
+  name: string;
+};
+
+export const collections: Collection[] = [
+  {
+    id: 1,
+    imageUrl: "https://demo.auctify.com/collection-images/collection_67c09767f420b.webp",
+    name: "Used Office Chairs",
+  },
+  {
+    id: 2,
+    imageUrl: "https://demo.auctify.com/collection-images/collection_67c0b48a7808c.webp",
+    name: "Used Refrigerators",
+  },
+  {
+    id: 3,
+    imageUrl: "https://demo.auctify.com/collection-images/collection_67c0b6a08a5d3.webp",
+    name: "Used Coffee Machines",
+  },
+  {
+    id: 4,
+    imageUrl: "https://demo.auctify.com/collection-images/collection_67c09767f420b.webp",
+    name: "Used Office Chairs",
+  },
+  {
+    id: 5,
+    imageUrl: "https://demo.auctify.com/collection-images/collection_67c0b48a7808c.webp",
+    name: "Used Refrigerators",
+  },
+  {
+    id: 6,
+    imageUrl: "https://demo.auctify.com/collection-images/collection_67c0b6a08a5d3.webp",
+    name: "Used Coffee Machines",
+  },
+];
