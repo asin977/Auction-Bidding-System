0f365b1fbce86a1a622cafe8cceaae27f57ee4f5
Assin Anna <assinannameckal@gmail.com>
2025-06-25 09:07:56 +0530

    refractor: formatted the code

--- src/components/signin/index.tsx
+++ src/components/signin/index.tsx
@@ -1,36 +1,36 @@
-import './index.css'
+import './index.css';
 
-export const SignIn =() => (
+export const SignIn = () => (
   <>
-    <div className='main-sign-container'>
-      <div className='sub-container'>
-        <div className='welcome-back'>
+    <div className="main-sign-container">
+      <div className="sub-container">
+        <div className="welcome-back">
           <h2>Welcome Back</h2>
         </div>
       </div>
-      <div className='sign-container'>
-        <h1 className='head'>Sign In</h1>
+      <div className="sign-container">
+        <h1 className="head">Sign In</h1>
         <div>
-          <form className='details-container'>
-            <input className='name-container'
-            type="text"
-            name='email'
-            placeholder='Enter your email *'
-            required
+          <form className="details-container">
+            <input
+              className="name-container"
+              type="text"
+              name="email"
+              placeholder="Enter your email *"
+              required
             />
-            <input className='details-container'
-            type='text'
-            name='password'
-            placeholder='Enter your Password *'
-            /> 
+            <input
+              className="details-container"
+              type="text"
+              name="password"
+              placeholder="Enter your Password *"
+            />
           </form>
-          <span className='forgot-password'>forgot password</span>
+          <span className="forgot-password">forgot password</span>
         </div>
-        <button className='sign-up'>Sign In</button>
-        <p className='sign-account'>New here?Create an Account</p>
+        <button className="sign-up">Sign In</button>
+        <p className="sign-account">New here?Create an Account</p>
       </div>
     </div>
   </>
-)
-  
-
+);
