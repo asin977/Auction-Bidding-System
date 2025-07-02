9a8f0649a87e6c1cea268fea3b2c657c99ba96b0
Assin Anna <assinannameckal@gmail.com>
2025-07-02 15:00:10 +0530

    refrcator:resolved review comments

--- src/components/loginButton/index.tsx
+++ src/components/loginButton/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+
 import './styles.css';
 
 const LoginButton = () => (
   <>
-    <button className='create-account'>Create Account</button>
+    <button className="create-account">Create Account</button>
     <button className="login">Login</button>
   </>
 );
